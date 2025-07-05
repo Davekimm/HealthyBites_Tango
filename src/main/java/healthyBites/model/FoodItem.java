25ae3631a2f5257fb7c8a68c90e5a9685e79cf82
hoanguyen <hoanguyen1797@gmail.com>
2025-07-05 19:43:16 -0400

    fixed some bugs

--- src/main/java/healthyBites/model/FoodItem.java
+++ src/main/java/healthyBites/model/FoodItem.java
@@ -4,12 +4,14 @@ package healthyBites.model;
  * Food item represents a class to store a food and its quantity
  */
 public class FoodItem {
-    public final String name;
-    public final double quantity;
+    private final String name;
+    private final double quantity;
+    private final String unit;
 
-    public FoodItem(String name, double quantity) {
+    public FoodItem(String name, double quantity, String unit) {
         this.name = name;
         this.quantity = quantity;
+        this.unit = unit;
     }
 
     public String getName() {
@@ -18,5 +20,23 @@ public class FoodItem {
 
     public double getQuantity() {
         return quantity;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public String toString() {
+        return quantity + " " + unit + " of " + name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FoodItem foodItem = (FoodItem) o;
+        return Double.compare(foodItem.quantity, quantity) == 0 &&
+               name.equals(foodItem.getName()) &&
+               unit.equals(foodItem.getUnit());
     }
 }

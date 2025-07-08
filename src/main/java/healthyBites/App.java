19b5a852c7138ea378a7144b0e7379e2b58e5ac1
ezraPk <ezrakal6@gmail.com>
2025-07-08 08:26:20 -0400

    Update App.java

--- src/main/java/healthyBites/App.java
+++ src/main/java/healthyBites/App.java
@@ -1,7 +1,29 @@
 package healthyBites;
 
+import javax.swing.SwingUtilities;
+
+import healthyBites.controller.Controller;
+import healthyBites.view.ViewFacade;
+import healthyBites.model.ConcreteModel;
+
+/**
+ * Main application entry point with Controller as observer
+ */
 public class App {
-	// public static void main(String[] args) {
-	// 	new Controller();
-	// }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            // Create the UI facade
+            ViewFacade viewFacade = new ViewFacade();
+
+            // Create the controller
+            Controller controller = new Controller(viewFacade);
+            
+            // Register Controller as observer with the Model
+            ConcreteModel model = ConcreteModel.getInstance();
+            model.addObserver(controller);
+                                 
+            // Show the application
+            viewFacade.showFrame();
+        });
+    }
 }
